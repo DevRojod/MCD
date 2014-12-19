30764af4fd67d72f05bb19bb0ae72e480b9aa012
Roderik Zandberg <Roderik@ro-dev.nl>
2014-12-19 05:15:11 +0100

    versie 1
    Creation that lead to the first version

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
-<html>
-<head>
-    <title>Such a title! Wow! So nice!</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <img src="images/kaart.jpg" border="1" />
-    <p><strong><?php echo $_GET["mdw"]; ?></strong></p>
-</body>
-</html>
+<form action="kaart.php">
+<fieldset>
+<legend>Gegevens:</legend>
+Voornaam:<br>
+<input type="text" name="mdw" value="" autofocus>
+<br><br>
+Afdeling:<br>
+<select name="deel">
+<option value="algemeen" selected>Algemeen</option>
+<option value="kw">Kruidenierswaren</option>
+<option value="zuivel">Zuivel</option>
+<option value="kassa">Kassa</option>
+<option value="agf">AGF</option>
+<option value="brood">Brood</option>
+<option value="vleeswaren">Vleeswaren & Kaas</option>
+<option value="slagerij">Slagerij</option>
+</select>
+<br><br>
+Type:<br>
+<select name="ext">
+<option value="jpg" selected>JPG</option>
+<option value="png">PNG</option>
+</select>
+<br><br><br>
+<input type="submit" value="Bouwen!"></fieldset>
+</form>
---
+++ kaart.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Such a title! Wow! So nice!</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <img src="http://dev.ro-dev.nl/MCD/images/<?php echo $_GET['ext']; ?>/<?php echo $_GET['deel']; ?>.<?php echo $_GET['ext']; ?>" border="1" />
+    <p><strong><?php echo $_GET["mdw"]; ?></strong></p>
+</body>
+</html>
